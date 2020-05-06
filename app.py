8b99d9c4702fcac42f6f79c44576c6af8e04b5bc
siranjeevi <brainkid92@gmail.com>
2020-05-06 15:01:42 +0530

    cors

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,  jsonify
 from flask_restful import Api
+from flask_cors import CORS
 from db import db
 
 from resource.shop import ShopCreate
@@ -7,6 +8,7 @@ from resource.shop import Shop
 from resource.shop import ShopList
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fbdehezqfnhjqd:79bcc943f58c12f74d8ae2892bc784afa6460bf24fa25ca5252e3debf88c8a54@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/d6mal2jruh45nd'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
