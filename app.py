8bc4e696129e52b5c2d796c1cbe822828859dc96
siranjeevi <brainkid92@gmail.com>
2020-05-06 11:23:52 +0530

    m

--- app.py
+++ app.py
@@ -13,16 +13,17 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# @app.before_first_request
+# def create_tables():
+    # db.create_all()
 
 api.add_resource(ShopCreate, '/api/create')
 api.add_resource(ShopList, '/api/list')
 api.add_resource(Shop, '/api/shop/<int:id>')
 
 if __name__ == '__main__':
+    db.create_all()
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    app.run()
 
 
